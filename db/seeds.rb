059f561ba8d4bd79225d779da332037a3cfbec2f
nataliavega <nvegar@gmail.com>
2022-05-09 08:25:04 +0200

    all lecture

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,21 @@
 # This is where you can create initial data for your app.
+# puts 'Creating restaurants...'
+# tour_d_argent = Restaurant.new(name: "La Tour d'Argent")
+# tour_d_argent.save!
+
+# chez_gladines = Restaurant.new(name: "Chez Gladines")
+# chez_gladines.save!
+# puts 'Finished!'
+
+require 'faker'
+
+puts 'Creating 100 fake restaurants...'
+100.times do
+  restaurant = Restaurant.new(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    rating:  rand(0..5)
+  )
+  restaurant.save!
+end
+puts 'Finished!'
